3a122465dd36befdd443797a2131dc3584690081
Andrew <reinforsed@gmail.com>
2024-02-15 18:06:12 +0200

    hw_2 done

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log('hello world');
+
+//First task
+let firstName = prompt('Enter your name');
+let surName = prompt('Enter your sure name');
+let age = prompt('Enter your age');
+
+alert(`Your name ${firstName}, your surename ${surName}, you are ${age} years old`);
+
+const result = firstName + " " + surName + " " + age;
+console.log(result);
+
+//Second task
+let number = 12345;
+let num = number.toString();
+console.log(num[4] + num[3] + num[2] + num[1] + num[0]);
