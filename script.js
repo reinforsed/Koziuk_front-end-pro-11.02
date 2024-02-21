d40e5dbdd27f5399db1c2347a311a5ba6ee52901
Andrey <reinforsed@gmail.com>
2024-02-21 15:13:18 +0200

    hw_3 ready

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstNumber = +prompt('Enter first number');
+let secondNumber = +prompt('Enter second number');
+
+const numAdditon = firstNumber + secondNumber;
+const numSubtraction = firstNumber - secondNumber;
+const numMultiplication = firstNumber * secondNumber;
+const numDivision = firstNumber / secondNumber;
+
+alert(`${firstNumber} + ${secondNumber} = ${numAdditon} \n${firstNumber} - ${secondNumber} = ${numSubtraction} \n${firstNumber} * ${secondNumber} = ${numMultiplication} \n${firstNumber} / ${secondNumber} = ${numDivision}`);
