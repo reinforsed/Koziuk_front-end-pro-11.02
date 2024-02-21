3dbb2c8da9d7dd462d47665ab4d8e189f62cc814
Andrey <reinforsed@gmail.com>
2024-02-21 15:32:34 +0200

    hw_5 is ready

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstNum = +prompt('Enter the first number');
+let secondNum = +prompt('Enter the second number');
+let thirdNum = +prompt('Enter the third number');
+
+const avarageNum = (firstNum + secondNum + thirdNum) / 3;
+
+alert(`The avarage number is ${avarageNum}`);
