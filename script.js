0f109165cf2a3a04de69c5b315f4f88bdbd3c84e
Andrey <reinforsed@gmail.com>
2024-04-09 13:41:34 +0300

    done

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const inputField = document.getElementById('myField');
+const myDiv = document.getElementById('myDiv');
 
 
+inputField.addEventListener('focus', function () {
+    myDiv.classList.remove('hidden');
+});
 
+inputField.addEventListener('blur', function () {
+    myDiv.classList.add('hidden');
+});
