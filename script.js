19f2072a6eaff66b532e067a62c0805d05645829
Andrey <reinforsed@gmail.com>
2024-04-09 14:13:00 +0300

    done

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const table = document.getElementById('myTable');
 
-
+let count = 1;
+for (let i = 0; i < 10; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < 10; j++) {
+        const cell = document.createElement('td');
+        cell.textContent = count;
+        row.appendChild(cell);
+        count++;
+    }
+    table.appendChild(row);
+}
 
